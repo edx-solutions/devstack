204b96ea08f84631cc9320457d2c21cc79f4afd2
Shafqat Farhan <shafqat.farhan@arbisoft.com>
2019-11-15 17:24:39 +0500

    Updated apros service name

--- provision-apros.sh
+++ provision-apros.sh
@@ -3,9 +3,9 @@ set -o pipefail
 set -x
 
 # Bring apros containers online
-docker-compose $DOCKER_COMPOSE_FILES up -d apros
+docker-compose $DOCKER_COMPOSE_FILES up -d mcka_apros
 
-docker-compose exec apros bash -c 'source /edx/app/apros/venvs/mcka_apros/bin/activate && cd /edx/app/apros/mcka_apros && python manage.py collectstatic --noinput'
-docker-compose exec apros bash -c 'source /edx/app/apros/venvs/mcka_apros/bin/activate && cd /edx/app/apros/mcka_apros && python manage.py migrate'
-docker-compose exec apros bash -c 'source /edx/app/apros/venvs/mcka_apros/bin/activate && cd /edx/app/apros/mcka_apros && pip install -r requirements.txt'
-docker-compose exec apros bash -c 'source /edx/app/apros/venvs/mcka_apros/bin/activate && cd /edx/app/apros/mcka_apros && python manage.py load_seed_data'
+docker-compose exec mcka_apros bash -c 'source /edx/app/apros/venvs/mcka_apros/bin/activate && cd /edx/app/apros/mcka_apros && pip install -r requirements.txt'
+docker-compose exec mcka_apros bash -c 'source /edx/app/apros/venvs/mcka_apros/bin/activate && cd /edx/app/apros/mcka_apros && python manage.py assets build'
+docker-compose exec mcka_apros bash -c 'source /edx/app/apros/venvs/mcka_apros/bin/activate && cd /edx/app/apros/mcka_apros && python manage.py collectstatic --noinput'
+docker-compose exec mcka_apros bash -c 'source /edx/app/apros/venvs/mcka_apros/bin/activate && cd /edx/app/apros/mcka_apros && python manage.py migrate'
--- provision.sql
+++ provision.sql
@@ -3,5 +3,6 @@ CREATE DATABASE IF NOT EXISTS edxapp_csmh;
 GRANT ALL ON edxapp.* TO 'edxapp001'@'%' IDENTIFIED BY 'password';
 GRANT ALL ON edxapp_csmh.* TO 'edxapp001'@'%';
 CREATE DATABASE IF NOT EXISTS mcka_apros;
+CREATE DATABASE IF NOT EXISTS edx;
 
 FLUSH PRIVILEGES;
