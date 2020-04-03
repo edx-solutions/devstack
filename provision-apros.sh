2cef96d306ce4b647158d0564567c396fed2c6c4
Shafqat Farhan <shafqat.farhan@arbisoft.com>
2020-04-03 20:19:58 +0500

    updated provision script

--- provision-apros.sh
+++ provision-apros.sh
@@ -11,3 +11,4 @@ docker-compose exec mcka_apros bash -c 'source /edx/app/apros/venvs/mcka_apros/b
 docker-compose exec mcka_apros bash -c 'source /edx/app/apros/venvs/mcka_apros/bin/activate && cd /edx/app/apros/mcka_apros && python manage.py assets build'
 docker-compose exec mcka_apros bash -c 'source /edx/app/apros/venvs/mcka_apros/bin/activate && cd /edx/app/apros/mcka_apros && python manage.py collectstatic --noinput'
 docker-compose exec mcka_apros bash -c 'source /edx/app/apros/venvs/mcka_apros/bin/activate && cd /edx/app/apros/mcka_apros && python manage.py migrate'
+docker-compose exec mcka_apros bash -c 'source /edx/app/apros/venvs/mcka_apros/bin/activate && cd /edx/app/apros/mcka_apros && python manage.py load_seed_data'
--- provision-lms.sh
+++ provision-lms.sh
@@ -5,8 +5,8 @@ set -x
 apps=( lms studio )
 
 # Load database dumps for the largest databases to save time
-#./load-db.sh edxapp
-#./load-db.sh edxapp_csmh
+./load-db.sh edxapp
+./load-db.sh edxapp_csmh
 
 # Bring edxapp containers online
 for app in "${apps[@]}"; do
@@ -18,18 +18,6 @@ docker-compose exec studio bash -c 'source /edx/app/edxapp/edxapp_env && cd /edx
 
 #Installing prereqs crashes the process
 docker-compose restart lms
-
-#TODO: Fix MySQL-python installation error: this is MySQLdb version (1, 2, 5, 'final', 1), but _mysql is version (1, 4, 4, 'final', 0)
-# This is a temporary fix, we will remove it once ironwood rebase is complete
-#docker-compose exec lms bash -c 'source /edx/app/edxapp/edxapp_env &&
-#                                  pip uninstall --disable-pip-version-check -y mysqlclient || true &&
-#                                  pip uninstall --disable-pip-version-check -y MySQL-python || true &&
-#                                  pip install --disable-pip-version-check MySQL-python'
-#
-#docker-compose exec studio bash -c 'source /edx/app/edxapp/edxapp_env &&
-#                                  pip uninstall --disable-pip-version-check -y mysqlclient || true &&
-#                                  pip uninstall --disable-pip-version-check -y MySQL-python || true &&
-#                                  pip install --disable-pip-version-check MySQL-python'
 
 # Run edxapp migrations first since they are needed for the service users and OAuth clients
 docker-compose exec lms bash -c 'source /edx/app/edxapp/edxapp_env && cd /edx/app/edxapp/edx-platform && NO_PREREQ_INSTALL=1 paver update_db --settings devstack_docker'
@@ -45,14 +33,6 @@ docker-compose exec lms bash -c '/edx/app/edx_ansible/venvs/edx_ansible/bin/ansi
 docker-compose exec lms bash -c 'rm /edx/app/edxapp/edx-platform/.prereqs_cache/Node_prereqs.sha1'
 
 # Fix Build failed running pavelib.assets.update_assets: Subprocess return code: 1
-##TODO: This is a temporary fix, we will remove it once ironwood rebase is complete
-#docker-compose exec lms bash -c 'source /edx/app/edxapp/edxapp_env &&
-#                                  pip uninstall --disable-pip-version-check -y djangorestframework || true &&
-#                                  pip install e git+https://github.com/edx/django-rest-framework.git@3c72cb5ee5baebc4328947371195eae2077197b0#egg=djangorestframework==3.2.3'
-#
-#docker-compose exec studio bash -c 'source /edx/app/edxapp/edxapp_env &&
-#                                  pip uninstall --disable-pip-version-check -y djangorestframework || true &&
-#                                  pip install e git+https://github.com/edx/django-rest-framework.git@3c72cb5ee5baebc4328947371195eae2077197b0#egg=djangorestframework==3.2.3'
 
 # Create static assets for both LMS and Studio
 for app in "${apps[@]}"; do
